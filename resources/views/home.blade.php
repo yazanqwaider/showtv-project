0d9a7fc4ba2376b4e159f4fe44ef612b9b4c2069
yazan qwaider <yazan.qwaider@joacademy.com>
2025-07-15 15:39:21 +0300

    Add the latest episodes in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,29 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style>
+        .episode-box {
+            width: 16%;
+            border-radius: 12px;
+            overflow: hidden;
+            background: #f7f7f7;
+            box-shadow: 0px 0px 8px #979797;
+        }
+
+        .episode-box img {
+            width: 100%;
+            height: 200px;
+            border-radius: 12px;
+            margin-bottom: 10px;
+        }
+
+        .episode-box:hover {
+            transition: 0.5s;
+            cursor: pointer;
+            transform: scale(1.06)
+        }
+    </style>
 </head>
 
 <body>
@@ -49,6 +72,22 @@
                 </li>
             @endauth
         </ul>
+    </div>
+
+
+    <div class="p-3">
+        <div>
+            <h4>Latest Episodes</h4>
+
+            <div class="d-flex gap-4">
+                @foreach ($latest_episodes as $latest_episode)
+                    <div class="episode-box">
+                        <img src="{{ url('storage/' . $latest_episode->thumbnail_url) }}">
+                        <h5 class="p-2">{{ $latest_episode->title }}</h5>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 
 </body>
